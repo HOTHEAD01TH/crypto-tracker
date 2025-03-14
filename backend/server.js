4e930e8e2e642cdc63b00a86d8e8e93c4f4314b8
HOTHEAD01TH <hothead01th@gmail.com>
2025-03-15 03:12:54 +0530

    / caused cors problem seriously bruhh

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'https://crypto-project-delta.vercel.app/',
+  origin: 'https://crypto-project-delta.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
